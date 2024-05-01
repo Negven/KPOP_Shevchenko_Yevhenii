a8af35bee9f770ed2d3aac1c352665c6a98065d9
Negven <35338243+Negven@users.noreply.github.com>
2024-05-01 11:36:06 +0200

    add 1-2 labs

---
+++ VTPS/lab2/index.js
@@ -0,0 +1,36 @@
+
+var modal = document.getElementById("myModal");
+var modalTeacher = document.getElementById("modalTeacher");
+var span = document.querySelectorAll(".close");
+
+window.onclick = function(event) {
+
+    if (event.target === modalTeacher || event.target===modal) {
+
+        modal.style.display = "none";
+        modalTeacher.style.display = "none";
+    }
+}
+
+
+document.querySelectorAll(".teacher_click").forEach(btn => {
+    btn.addEventListener('click',()=>{
+        console.log(btn)
+        modalTeacher.style.display = "block";
+    })
+})
+
+document.querySelectorAll(".modal-toggle").forEach(btn => {
+    btn.addEventListener('click',()=>{
+        modal.style.display = "block";
+    })
+})
+
+
+span.forEach(close => close.addEventListener( "click",
+    () => {
+        modal.style.display = "none";
+        modalTeacher.style.display = "none";
+    }
+))
+
